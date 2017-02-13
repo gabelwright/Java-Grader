6296509f5734a400bacffb90970ee281bc4e1bbc
Gabel Wright <acronymcreations@gmail.com>
2017-02-13 15:11:58 -0700

    refactor: Admin areas better hidden from non-admin
    Using the admin boolean, admin areas are not immediate visible to
    non-admin users.
    Adds a db_manipulate file to perform common db tasks such as
    changing admin status, deleting users, etc.

---
+++ db_manipulation.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base, User, Assignment, Test, Post
+
+# engine = create_engine('postgresql://db:dbpass@localhost/db')
+engine = create_engine('sqlite:///codin-site.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Promote user to admin
+# USER_TO_PROMOTE = 'username goes here'
+# user = session.query(User).filter(User.username == USER_TO_PROMOTE).first()
+# if user:
+# 	user.admin = True
+# 	session.commit()
+
+
+# Demote user from admin
+# USER_TO_DEMOTE = 'username goes here'
+# user = session.query(User).filter(User.username == USER_TO_PROMOTE).first()
+# if user:
+# 	user.admin = False
+# 	session.commit()
+
+
+#Delete user
+# USER_TO_DELETE = 'username goes here'
+# user = session.query(USER).filter(User.username == USER_TO_DELETE).first()
+# if user:
+# 	session.delete(user)
+# 	session.commit()
+
+
+# Delete all data in database
+# us = session.query(Test).delete()
+# print 'deleted %s rows from Test' % us
+# session.commit()
+# us = session.query(Assignment).delete()
+# print 'deleted %s rows from Test' % us
+# session.commit()
+# us = session.query(Post).delete()
+# print 'deleted %s rows from Test' % us
+# session.commit()
+# us = session.query(User).delete()
+# print 'deleted %s rows from Test' % us
+# session.commit()
+
+
+
+# Drop all tables
+# Base.metadata.drop_all(engine)
